d3505f91f93259af7af375657e4d57b93d43af04
minikupa <znvk3768@gmail.com>
2020-05-22 13:34:05 +0900

    트게더 링크, 저작권 표시

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'widget/Hanwha.dart';
 import 'widget/Tajiri.dart';
 
@@ -18,26 +20,58 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       home: Scaffold(
           body: Stack(
-            children: <Widget>[
-              Positioned.fill(child: Image.asset(
-                "assets/background.webp",
-                width: double.infinity,
-                height: double.infinity,
-                fit: BoxFit.cover,
-              )),
-              Kane(),
-              Hanwha(),
-              Tajiri(),
-              Align(
-                alignment: Alignment(-1.0, 1.05),
-                child: Image.asset(
-                  "assets/mushroom.webp",
-                  width: 70,
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
+        children: <Widget>[
+          Positioned.fill(
+              child: Image.asset(
+            "assets/background.webp",
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
           )),
+          Kane(),
+          Hanwha(),
+          Tajiri(),
+          Align(
+            alignment: Alignment(-1.0, 1.05),
+            child: Image.asset(
+              "assets/mushroom.webp",
+              width: 70,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            margin: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                InkWell(
+                    child: Image.asset(
+                      "assets/tgd.webp",
+                      width: 20,
+                    ),
+                    onTap: () => _launchURL("https://tgd.kr/kanetv8")),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: InkWell(
+                    child: Image.asset(
+                      "assets/copyright.webp",
+                      width: 20,
+                    ),
+                    onTap: () => _launchURL("https://minikupa.com/142"),
+                  ),
+                )
+              ],
+              mainAxisSize: MainAxisSize.min,
+            ),
+          )
+        ],
+      )),
     );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
   }
 }
